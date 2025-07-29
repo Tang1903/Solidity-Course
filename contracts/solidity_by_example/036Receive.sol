f9e2417d96c6508ac25a0a888ad2426f24986eee
Tang1903 <246968103@qq.com>
2025-07-29 23:56:44 +0800

    receive 函数实现自动接收和处理 ETH

--- contracts/solidity_by_example/036Receive.sol
+++ contracts/solidity_by_example/036Receive.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.13;
 
 contract ReceiveFunction {
     // 1、定义 Receive 事件
-    event ReceiveETH(address Sender, uint Value);
+    event Receive(address Sender, uint Value);
 
     // 2、接收 ETH 时， 触发 Receive事件 来记录 合约接收到的以太币数量、发送者地址 等信息。
     receive() external payable {
-
-        emit ReceiveETH(msg.sender, msg.value);
+        emit Receive(msg.sender, msg.value);
 
         // 处理没有数据的 ETH 转账
     }
@@ -17,13 +16,11 @@ contract ReceiveFunction {
 
 // 在 receive() 函数 内部编写 更新合约的状态 的 逻辑，来处理 接收到 的 以太币ETH
 contract SimpleReceive {
-
     // 1、定义 状态变量
     uint256 public totalReceived;
 
     // 2、接收 ETH 时，将 接收到的以太币数量 累加到 totalReceived变量 中
     receive() external payable {
-        
         totalReceived += msg.value;
 
         // 处理没有数据的 ETH 转账
