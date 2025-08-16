93351ecdebfb8c66b3a1da0d375f581f5cf74322
Tang1903 <246968103@qq.com>
2025-08-16 21:26:10 +0800

    合约的动态创建和销毁过程

--- contracts/solidity_by_example/046contract-Create-Destruct.sol
+++ contracts/solidity_by_example/046contract-Create-Destruct.sol
@@ -5,27 +5,23 @@ pragma solidity ^0.8.13;
 import "./045Selfdestruct.sol";
 
 contract DeployContract {
-
-    // 将 被用于 demo函数 的 返回值
+    // 被用于 demo函数 的 返回值
     struct DemoResult {
         address addr;
         uint balance;
         uint value;
     }
 
-    // 构造函数 被标记为 payable，这意味着 该合约在部署时 可以向其 发送以太币，然而，这里构造函数内部并没有执行任何操作。
+    // 构造函数 被标记为 payable，这意味着 在部署合约时 可以向其 发送以太币。然而，构造函数内部并没有执行任何操作。
     constructor() payable {}
 
-    // 被标记为view 意味着 它不会修改 区块链上的任何状态，它会返回 当前合约的以太币余额。
+    // 被标记为view 意味着 它不会修改 区块链上的任何状态。它返回当前合约的以太币余额。
     function getBalance() external view returns (uint balance) {
-
         balance = address(this).balance;
-
     }
 
     function demo() public payable returns (DemoResult memory) {
-
-        // 使用 msg.value（即：调用者发送的以太币数量）部署 一个新的DeleteContract实例，并将其地址存储在 变量del 中。
+        // 使用 msg.value（即调用者发送的以太币数量）部署一个新的DeleteContract实例，并将其地址存储在变量del中。
         DeleteContract del = new DeleteContract{value: msg.value}();
 
         DemoResult memory res = DemoResult({
@@ -39,3 +35,12 @@ contract DeployContract {
         return res;
     }
 }
+
+// demo函数 是 公共可调用的，并且也是payable的。这个函数执行以下操作：
+// 1、使用msg.value（即调用者发送的以太币数量）部署一个新的DeleteContract实例，并将其地址存储在变量del中。
+// 2、创建一个DemoResult类型的局部变量res，并初始化其字段：
+//      addr字段设置为新部署的DeleteContract实例的地址。
+//      balance字段通过调用del.getBalance()获取DeleteContract实例的余额（这里假设DeleteContract有一个返回余额的函数getBalance）。
+//      value字段通过调用del.value()获取（这里假设DeleteContract有一个返回某种值的函数value，但具体返回什么值取决于DeleteContract的实现）。
+// 3、调用del.deleteContract()（这里假设DeleteContract有一个deleteContract函数用于删除或自毁合约）。
+// 4、返回res结构体实例。
